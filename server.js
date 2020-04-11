3f0df72bc62a6271e31ddb1a8434414ab9bc2a43
carmelolg <carmelolagamba@hotmail.it>
2020-04-11 16:35:17 +0200

    assertLink

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app = express();
 
 app.use(express.static('./dist'));
 
+
+app.get('/.well-known/assetlinks.json', function(req, res, next) {
+    res.set('Content-Type', 'application/json');
+    res.status(200).send('./.well-known/assetlinks.json');
+});
+
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/'}),
 );
